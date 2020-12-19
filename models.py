0cb5f7554e65ce04a2e6da16f66cd96ee8ab0a1d
Faisal Alqarni <Falqarni@mrsool.co>
2020-12-20 02:42:04 +0300

    updating the models

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ db = SQLAlchemy()
 
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     migrate = Migrate(app, db)
     db.app = app
     db.init_app(app)
@@ -29,8 +29,58 @@ def setup_db(app):
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+    db_seeding()
 
-
+def db_seeding():
+    movie1 = Movie(title = "First Movie", release_date = "10/10/2010")
+    movie2 = Movie(title = "Second Movie", release_date = "1/12/2015")
+    movie3 = Movie(title = "Final Movie", release_date = "1/10/2018")
+    actor1 = Actor(name = "First", age = 18, gender = "male")
+    actor2 = Actor(name = "Second", age = 25, gender = "male")
+    actor3 = Actor(name = "Third", age = 40, gender = "male")
+    actor4 = Actor(name = "Thirds", age = 13, gender = "female")
+    actor5 = Actor(name = "wally", age = 16, gender = "femal")
+    actor6 = Actor(name = "jhon", age = 20, gender = "male")
+    
+    Helpers.insert(movie1)
+    Helpers.insert(movie2)
+    Helpers.insert(movie3)
+    Helpers.insert(actor1)
+    Helpers.insert(actor2)
+    Helpers.insert(actor3)
+    Helpers.insert(actor4)
+    Helpers.insert(actor5)
+    Helpers.insert(actor6)
+    
+    association1 = association.insert().values(movie_id=movie1.id, actor_id=actor1.id)    
+    association2 = association.insert().values(movie_id=movie1.id, actor_id=actor2.id)    
+    association3 = association.insert().values(movie_id=movie1.id, actor_id=actor3.id)    
+    association4 = association.insert().values(movie_id=movie1.id, actor_id=actor4.id)    
+    association5 = association.insert().values(movie_id=movie1.id, actor_id=actor5.id)    
+    association6 = association.insert().values(movie_id=movie1.id, actor_id=actor6.id)  
+      
+    association7 = association.insert().values(movie_id=movie2.id, actor_id=actor1.id)    
+    association8 = association.insert().values(movie_id=movie2.id, actor_id=actor2.id)    
+    association9 = association.insert().values(movie_id=movie2.id, actor_id=actor3.id)
+    
+    association10 = association.insert().values(movie_id=movie3.id, actor_id=actor4.id)    
+    association11 = association.insert().values(movie_id=movie3.id, actor_id=actor5.id)    
+    association12 = association.insert().values(movie_id=movie3.id, actor_id=actor6.id)
+    
+    
+    db.session.execute(association1)    
+    db.session.execute(association2)    
+    db.session.execute(association3)    
+    db.session.execute(association4)    
+    db.session.execute(association5)    
+    db.session.execute(association6)    
+    db.session.execute(association7)    
+    db.session.execute(association8)    
+    db.session.execute(association9)    
+    db.session.execute(association10)    
+    db.session.execute(association11)    
+    db.session.execute(association12)    
+    db.session.commit()
 
 # ==========================================
 # BASE
@@ -63,30 +113,6 @@ class Helpers():
         db.session.add(self)
         db.session.commit()
 
-
-    '''
-    insert()
-        inserts a new model into a database
-        the model must have a unique name
-        the model must have a unique id or null id
-        EXAMPLE
-            movie = Movie(title=req_title, release_date=req_release_date)
-            actor = Actor(...)
-            actor = Actor(...)
-            Helpers.assign_actors_to_movie([1,2],1)
-    '''
-    def assign_actors_to_movie(actor_ids, movie_id):
-        actors = db.session.query(Actor).filter(Actor.id.in_(actor_ids)).all()
-        movie = db.session.query(Movie).filter(Movie.id == movie_id).first()
-        movie.actors = [actor for actor in actors]
-        db.session.commit()
-        
-    def assign_movies_to_actor(movie_ids, actor_id):
-        movies = db.session.query(Movie).filter(Movie.id.in_(movie_ids)).all()
-        actors = db.session.query(Actor).filter(Actor.id == actor_id).first()
-        actors.movies = [movie for movie in movies]
-        db.session.commit()
-    
     '''
     update()
         updates a new model from database
@@ -117,12 +143,12 @@ class Helpers():
 # MODELS
 # ==========================================
 '''
-association_table
+association
 an association table, to apply many-to-many relationship
 '''
-association_table = db.Table('association', Base.metadata,
-    Column('movies_id', Integer, db.ForeignKey('movies.id')),
-    Column('actors_id', Integer, db.ForeignKey('actors.id'))
+association = db.Table('association', Base.metadata,
+    Column('movie_id', Integer, db.ForeignKey('movies.id')),
+    Column('actor_id', Integer, db.ForeignKey('actors.id'))
 )
 
 '''
@@ -134,8 +160,7 @@ class Movie(db.Model):
     id = Column(Integer(), primary_key=True)
     title = Column(String(), nullable=False)
     release_date =  Column(String(180), nullable=False)
-    # actors = db.relationship('Actor', backref='movies', lazy=True)
-    children = db.relationship('Actor', secondary = association_table, back_populates= 'movies')
+    actors = db.relationship('Actor', secondary = association, backref=db.backref('associations', lazy='joined'))
 
     def __init__(self, title,  release_date):
         self.title = title
@@ -146,7 +171,7 @@ class Movie(db.Model):
             'id': self.id,
             'title': self.title,
             'release_date': self.release_date,
-            'actors': json.loads(self.actors)
+            'actors': [actor.name for actor in self.actors]
         }
 
     def __repr__(self):
@@ -161,9 +186,8 @@ class Actor(db.Model):
     id = Column(Integer(), primary_key=True)
     name = Column(String(), nullable=False)
     age =  Column(Integer(), nullable=False)
-    gender =  Column(String(), nullable=False)
-    # movie = db.Column(db.Integer, db.ForeignKey('movies.id'))
-    movies = db.relationship('movies', secondary = association_table, back_populates= 'actors')
+    gender =  Column(String(10), nullable=False)
+    movies = db.relationship('Movie', secondary = association, backref=db.backref('associations', lazy='joined'))
     
     def short(self):
         return {
@@ -171,7 +195,7 @@ class Actor(db.Model):
             'name': self.name,
             'age': self.age,
             'gender': self.gender,
-            'recipe': json.loads(self.movie)
+            'movies': [movie.title for movie in self.movies]
         }
 
     def __repr__(self):
