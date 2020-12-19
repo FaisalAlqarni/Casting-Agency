d6882dc27dea5ffe91bc907cbe2c08c6d9bd3db3
Faisal Alqarni <Falqarni@mrsool.co>
2020-12-20 02:42:24 +0300

    implementing the endpoints (draft)

--- app.py
+++ app.py
@@ -23,10 +23,6 @@ db_drop_and_create_all()
 
 
 # ==========================================
-# SEEDING
-# ==========================================
-
-# ==========================================
 # ROUTES
 # ==========================================
 @app.route('/movies', methods=['GET'])
@@ -47,134 +43,165 @@ def get_actors():
         'Actors': actors_short
     })
 
-
-# @app.route('/movies/<int:id>', methods=['DELETE'])
-# # @requires_auth('delete:movies')
-# def delete_actor(payload, id):
-#     movie = Movie.query.filter_by(id=id).first()
-#     if not movie:
-#         return jsonify({'message': 'Movie not found.'})
-#     movie.delete()
-#     return jsonify({
-#         'message': movie.id + ' Deleted.',
-#         'success': True,
-#     }), 200
-
+@app.route('/movies/<int:id>', methods=['GET'])
+# @requires_auth('delete:movies')
+def show_movie(id):
+    movie = Movie.query.filter_by(id=id).first()
+    if not movie:
+        return abort(404)
+    return jsonify({
+        'success': True,
+        'data': movie.short()
+    }), 200
     
-# @app.route('/actors/<int:id>', methods=['DELETE'])
-# # @requires_auth('delete:actors')
-# def delete_actor(payload, id):
-#     actor = Actor.query.filter_by(id=id).first()
-#     if not actor:
-#         return jsonify({'message': 'Actor not found.'})
-#     actor.delete()
-#     return jsonify({
-#         'message': actor.id + ' Deleted.',
-#         'success': True,
-#     }), 200
+@app.route('/actors/<int:id>', methods=['GET'])
+# @requires_auth('delete:actors')
+def show_actor(id):
+    actor = Actor.query.filter_by(id=id).first()
+    if not actor:
+        return abort(404)
+    return jsonify({
+        'success': True,
+        'data': actor.short()
+    }), 200
 
-
-# @app.route('/movies', methods=['POST'])
-# # @requires_auth('post:movies')
-# def add_actor(payload):
-#     body = request.get_json()
-#     new_actor = Actor(
-#         name=body.get('name'),
-#         title=body.get('age'),
-#     )
-#     new_actor.save()
-#     return jsonify({
-#         'actor': new_actor.to_json(),
-#         'success': True
-#     }), 201
+@app.route('/movies/<int:id>', methods=['DELETE'])
+# @requires_auth('delete:movies')
+def delete_movie(id):
+    movie = Movie.query.filter_by(id=id).first()
+    if not movie:
+        return abort(404)
+    Helpers.delete(movie)
+    return jsonify({
+        'success': True,
+        'message': movie.id + ' is Deleted.',
+    }), 200
     
-# @app.route('/actors', methods=['POST'])
-# # @requires_auth('post:actors')
-# def add_actor(payload):
-#     """Handles POST requests for actors.
-#     returns:
-#         - actor object
-#         - success message
-#     """
-#     body = request.get_json()
-#     new_actor = Actor(
-#         name=body.get('name'),
-#         bio=body.get('bio'),
-#         age=body.get('age'),
-#         gender=body.get('gender'),
-#         movie=body.get('movie')
-#     )
-#     new_actor.save()
-#     return jsonify({
-#         'actor': new_actor.to_json(),
-#         'success': True
-#     }), 201
+@app.route('/actors/<int:id>', methods=['DELETE'])
+# @requires_auth('delete:actors')
+def delete_actor(id):
+    actor = Actor.query.filter_by(id=id).first()
+    if not actor:
+        return abort(404)
+    Helpers.delete(actor)
+    return jsonify({
+        'success': True,
+        'message': actor.id + ' is Deleted.',
+    }), 200
 
+@app.route('/actors', methods=['POST'])
+# @requires_auth('post:movies')
+def add_actor():
+    body = request.get_json()
+    name = body.get('name', None)
+    age = body.get('age', None)
+    gender = body.get('gender', None)
+    
+    if ((name is None) or (age is None) or (gender is None)):
+        return abort(422)
+    try:
+        new_actor = Actor(name = name, age = age, gender = gender)
+        Helpers.insert(new_actor)
+        return jsonify({
+            'success': True,
+            'actor': new_actor.short()
+        }), 201
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
+    
+@app.route('/movies', methods=['POST'])
+# @requires_auth('post:movies')
+def add_movie():
+    body = request.get_json()
+    title = body.get('title', None)
+    release_date = body.get("release_date", None)
+    
+    if ((title is None) or (release_date is None)):
+        return abort(422)
+    try:
+        new_movie = Movie(title = title, release_date = release_date)
+        Helpers.insert(new_movie)
+        return jsonify({
+            'success': True,
+            'actor': new_movie.short()
+        }), 201
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
 
-# @app.route('/drinks', methods=['POST'])
-# # @requires_auth('post:drinks')
-# def create_drinks():
-#     try:
-#         request_json = request.get_json()
-#         new_drink = Drink(title=request_json['title'], recipe=json.dumps(
-#             [request_json['recipe']]))
-#         new_drink.insert()
-#         drink = [new_drink.long()]
-#         return jsonify({
-#             'success': True,
-#             'drinks': drink
-#         })
-#     except Exception:
-#         abort(500)
-
-
-
-# @app.route('/drinks/<int:id>', methods=['PATCH'])
+@app.route('/movies/<int:id>', methods=['PATCH'])
 # @requires_auth('patch:drinks')
-# def update_drink(id):
-#     the_drink = Drink.query.filter(Drink.id == id).one_or_none()
-#     if the_drink is None:
-#         abort(404)
-#     the_drink.update()
-#     drink = [the_drink.long()]
-#     return jsonify({
-#         'success': True,
-#         'drinks': drink
-#     })
+def update_movie(self, id):
+    the_movie = Movie.query.filter(Movie.id == id).one_or_none()
+    if the_movie is None:
+        return abort(404)
+    
+    body = request.get_json()
+    title = body.get('title', None)
+    release_date = body.get("release_date", None)
+    
+    if ((title is None) and (release_date is None)):
+        return abort(422)
+    try:
+        if title is not None:
+            the_movie.title = title
+        if release_date is not None:
+            the_movie.release_date = release_date
+        
+        Helpers.update(the_movie)
+        return jsonify({
+            'success': True,
+            'actor': the_movie.short()
+        }), 201
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
+        
+@app.route('/actors/<int:id>', methods=['PATCH'])
+# @requires_auth('patch:drinks')
+def update_actor(self, id):
+    the_actor = Actor.query.filter(Actor.id == id).one_or_none()
+    if the_actor is None:
+        return abort(404)
+    
+    body = request.get_json()
+    name = body.get('name', None)
+    age = body.get("age", None)
+    gender = body.get("gender", None)
+    
+    if ((name is None) and (age is None) and (gender is None)):
+        return abort(422)
+    try:
+        if name is not None:
+            the_actor.name = name
+        if age is not None:
+            the_actor.age = age
+        if gender is not None:
+            the_actor.gender = gender
+        
+        Helpers.update(the_actor)
+        return jsonify({
+            'success': True,
+            'actor': the_actor.short()
+        }), 201
+    except:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
 
 
-# '''
-# @TODOx implement endpoint
-#     DELETE /drinks/<id>
-#         where <id> is the existing model id
-#         it should respond with a 404 error if <id> is not found
-#         it should delete the corresponding row for <id>
-#         it should require the 'delete:drinks' permission
-#     returns status code 200 and json {"success": True, "delete": id}
-#     where id is the id of the deleted record
-#         or appropriate status code indicating reason for failure
-# '''
-
-
-# @app.route('/drinks/<int:id>', methods=['DELETE'])
-# @requires_auth('delete:drinks')
-# def delete_drink(id):
-#     drink = Drink.query.filter(Drink.id == id).one_or_none()
-#     if drink is None:
-#         abort(404)
-#     drink.delete()
-#     return jsonify({
-#         'success': True,
-#         'delete': id
-#     })
 
 # ==========================================
 # Error Handling
 # ==========================================
-'''
-Example error handling for unprocessable entity
-'''
-
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -184,24 +211,6 @@ def unprocessable(error):
         "message": "unprocessable"
     }), 422
 
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-        jsonify({
-            "success": False,
-            "error": 404,
-            "message": "resource not found"
-            }), 404
-
-'''
-
-'''
-@TODOx implement error handler for 404
-    error handler should conform to general task above
-'''
-
-
 @app.errorhandler(404)
 def resource_not_found(error):
     return jsonify({
@@ -210,7 +219,6 @@ def resource_not_found(error):
         "message": "RECORD NOT FOUND"
     }), 404
 
-
 @app.errorhandler(500)
 def server_error(error):
     return jsonify({
@@ -218,13 +226,6 @@ def server_error(error):
         "error": 500,
         "message": "SERVER ERROR"
     }), 500
-
-
-'''
-@TODOx implement error handler for AuthError
-    error handler should conform to general task above
-'''
-
 
 @app.errorhandler(AuthError)
 def auth_error(error):
