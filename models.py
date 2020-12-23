9de28bed3f834ebf9961960c49cd95bf30c832d0
Faisal Alqarni <Falqarni@mrsool.co>
2020-12-23 04:46:13 +0300

    support providing database path

--- models.py
+++ models.py
@@ -18,12 +18,13 @@ database_path = 'postgres://jwwsigkdbkpuzf:67217f9ad3ba2ccc05c9b584505a7c9387884
 db = SQLAlchemy()
 
 
-def setup_db(app):
+def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    migrate = Migrate(app, db)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
+
 
 
 def db_drop_and_create_all():
